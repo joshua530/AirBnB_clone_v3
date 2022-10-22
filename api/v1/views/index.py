dc4794a7040a1b13dcea23785012f1a04c556de7
joshua530 <joshuaomari5@gmail.com>
2022-10-22 22:26:03 +0300

    make pep8 compliant

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -20,12 +20,12 @@ def status():
 @app_views.route('/stats')
 def stats():
     '''fetches stats'''
-    data= {
-        "amenities": storage.count(Amenity), 
-        "cities": storage.count(City), 
-        "places": storage.count(Place), 
-        "reviews": storage.count(Review), 
-        "states": storage.count(State), 
+    data = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
         "users": storage.count(User)
     }
     return jsonify(data)
