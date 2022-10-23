6c2b31af2e791afa02dfc6cedfd76e3e06cb4dc4
joshua530 <joshuaomari5@gmail.com>
2022-10-23 17:26:14 +0300

    delete extra line

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,7 +13,6 @@ HBNB_API_HOST = os.getenv('HBNB_API_HOST', '0.0.0.0')
 HBNB_API_PORT = int(os.getenv('HBNB_API_PORT', '5000'))
 
 
-
 @app.teardown_appcontext
 def teardown(self):
     '''close query after each session'''
