f8d325f6b31a93b6696462e2b12adcc455cf20ee
joshua530 <joshuaomari5@gmail.com>
2022-10-23 17:54:54 +0300

    implement cors

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,12 +2,14 @@
 '''api entry point'''
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
+from flask_cors import CORS
 from models import storage
 import os
 
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+CORS(app, origins='0.0.0.0')
 app.register_blueprint(app_views)
 HBNB_API_HOST = os.getenv('HBNB_API_HOST', '0.0.0.0')
 HBNB_API_PORT = int(os.getenv('HBNB_API_PORT', '5000'))
